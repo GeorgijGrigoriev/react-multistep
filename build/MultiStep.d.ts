29549b40ee6f7a1538adec6c8fdd289f9f6bcc80
Georgiy Grigorev <g.grigorev@vk.team>
2024-06-03 16:58:18 +0300

    build

---
+++ build/MultiStep.d.ts
@@ -0,0 +1,3 @@
+import { MultiStepProps } from './interfaces';
+export default function MultiStep(props: MultiStepProps): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=MultiStep.d.ts.map
---
+++ build/baseStyles.d.ts
@@ -0,0 +1,66 @@
+export namespace BaseStyles {
+    export { component };
+    export { section };
+    export { topNav };
+    export { topNavStep };
+    export { prevButton };
+    export { nextButton };
+    export { todo };
+    export { doing };
+}
+declare namespace component {
+    let marginRight: string;
+    let marginBottom: string;
+    let backgroundColor: string;
+    let maxWidth: string;
+}
+declare namespace section {
+    let display: string;
+    let justifyContent: string;
+    let margin: string;
+}
+declare namespace topNav {
+    let paddingTop: string;
+    let listStyleType: string;
+    let borderBottom: string;
+}
+declare namespace topNavStep {
+    let color: string;
+    let cursor: string;
+}
+declare namespace prevButton {
+    let color_1: string;
+    export { color_1 as color };
+    let backgroundColor_1: string;
+    export { backgroundColor_1 as backgroundColor };
+    export let border: string;
+    export let fontSize: string;
+    export let marginLeft: string;
+    let paddingTop_1: string;
+    export { paddingTop_1 as paddingTop };
+}
+declare namespace nextButton {
+    let color_2: string;
+    export { color_2 as color };
+    let backgroundColor_2: string;
+    export { backgroundColor_2 as backgroundColor };
+    let border_1: string;
+    export { border_1 as border };
+    let fontSize_1: string;
+    export { fontSize_1 as fontSize };
+    let marginRight_1: string;
+    export { marginRight_1 as marginRight };
+    let paddingTop_2: string;
+    export { paddingTop_2 as paddingTop };
+    export let float: string;
+}
+declare namespace todo {
+    let color_3: string;
+    export { color_3 as color };
+}
+declare namespace doing {
+    let color_4: string;
+    export { color_4 as color };
+}
+export {};
+//# sourceMappingURL=baseStyles.d.ts.map
