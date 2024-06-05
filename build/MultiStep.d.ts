da9f48d5bab4a081454aab46044ededda99d621b
Georgiy Grigorev <g.grigorev@vk.team>
2024-06-05 16:26:32 +0300

    update

--- build/MultiStep.d.ts
+++
@@ -1,3 +0,0 @@
-import { MultiStepProps } from './interfaces';
-export default function MultiStep(props: MultiStepProps): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=MultiStep.d.ts.map
